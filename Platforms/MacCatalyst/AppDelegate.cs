d6e1636518e07573060157c57e2bd9187958f949
Luis Capelo <luis.capelo@solarvista.com>
2025-03-17 09:13:16 +0000

    sample project

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Maui28414;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
